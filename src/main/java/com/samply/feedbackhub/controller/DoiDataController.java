d71bca110ad3583d2634a15aaada836dbee31802
Adam Repaský <adamson2326@gmail.com>
2023-04-27 22:57:44 +0200

    refactor: split createDoiData method

--- src/main/java/com/samply/feedbackhub/controller/DoiDataController.java
+++ src/main/java/com/samply/feedbackhub/controller/DoiDataController.java
@@ -34,38 +34,56 @@ public class DoiDataController {
     // Create a new DoiData
     @CrossOrigin(origins = "http://localhost:9000")
     @PostMapping("/doi-data")
-    public DoiData createDoiData(@Valid @RequestBody DoiData doi_data) throws DoiDataAlreadyPresentException {
-        if (doiDataRepository.findByRequest(doi_data.getRequestID()).size() > 0) {
-            throw new DoiDataAlreadyPresentException(doi_data.getRequestID());
+    public DoiData createDoiData(@Valid @RequestBody DoiData dataWithDoi) throws DoiDataAlreadyPresentException {
+        // Check if the DoiData is already present in the repository
+        if (doiDataRepository.findByRequest(dataWithDoi.getRequestID()).size() > 0) {
+            throw new DoiDataAlreadyPresentException(dataWithDoi.getRequestID());
         }
-        final Key key = Key.generateKey();
-        doi_data.setSymEncKey(key.serialise());
-        DoiData returnData = doiDataRepository.save(doi_data);
 
+        // Generate the symmetric encryption key
+        final Key key = Key.generateKey();
+        dataWithDoi.setSymEncKey(key.serialise());
+
+        // Create and send the BeamTask to the proxy server
+        BeamTask task = createBeamTask(dataWithDoi);
+
+        sendBeamTask(task);
+
+        // Hide the encryption key before returning as it is sensitive data
+        doiDataRepository.save(dataWithDoi);
+        dataWithDoi.setSymEncKey("hidden");
+        return dataWithDoi;
+    }
+
+    private BeamTask createBeamTask(DoiData dataWithDoi) {
         BeamTask task = new BeamTask();
         task.setFrom("app1.proxy1.broker");
 
         LinkedList<String> toList = new LinkedList<>();
         toList.add("app1.proxy2.broker");
         task.setTo(toList);
-        task.setBody(doi_data.getSymEncKey());
+
+        JSONObject bodyJson = new JSONObject();
+        bodyJson.put("doi", dataWithDoi.getSymEncKey());
+        bodyJson.put("requestId", dataWithDoi.getRequestID());
+        task.setBody(bodyJson.toString());
+
         task.setBackoffMillisecs(1000);
         task.setMaxTries(5);
         task.setTtl("30s");
-        task.setMetadata(doi_data.getRequestID());
+        task.setMetadata(null);
+        return task;
+    }
 
+    private JSONObject sendBeamTask(BeamTask task) {
         final String uri = "http://dev_proxy1_1:8081/v1/tasks";
         RestTemplate restTemplate = new RestTemplate();
+
         HttpHeaders headers = new HttpHeaders();
         headers.set("Authorization", "ApiKey app1.proxy1.broker App1Secret");
-
         HttpEntity<JSONObject> request = new HttpEntity<>(task.buildJSON(), headers);
-        System.out.println(request);
-        JSONObject result = restTemplate.postForObject(uri, request, JSONObject.class);
-        System.out.println(result);
 
-        returnData.setSymEncKey("hidden");
-        return returnData;
+        return restTemplate.postForObject(uri, request, JSONObject.class);
     }
 
     // Get a Single DoiData
@@ -75,9 +93,14 @@ public class DoiDataController {
         return doiDataRepository.findById(specimenFeedbackId)
                 .orElseThrow(() -> new DoiDataNotFoundException(specimenFeedbackId));
     }*/
-
-    // Get SpecimenFeedbacks by request ID
-    @CrossOrigin(origins = "http://localhost:9000")
+    // Get symmetricaly encrypted Doi by request ID
+    /*@GetMapping("/doi-token/{sym_key}")
+    public String getDoiTokenBySymKey(@PathVariable(value = "sym_key") String requestId) throws DoiDataNotFoundException {
+        List<DoiData> data = doiDataRepository.findByRequest(requestId);
+        if (data.size() == 0) throw new DoiDataNotFoundException(requestId);
+        return Token.generate(new Key(data.get(0).getSymEncKey()), data.get(0).getPublicationReference()).serialise();
+    }*/
+    // Get Doi by request ID
     @GetMapping("/doi-token/{req_id}")
     public String getDoiTokenByRequestID(@PathVariable(value = "req_id") String requestId) throws DoiDataNotFoundException {
         List<DoiData> data = doiDataRepository.findByRequest(requestId);
